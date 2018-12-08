44c3bd96e7f880585987927cd0c5256656d3e2e3
Mike Mehl <michaelalanmehl@gmail.com>
2018-12-08 11:02:14 -0500

    Delete HelloWorld.py

--- Backend/ApiClasses/HelloWorld.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-from flask import jsonify
-
-# Sample resource
-class HelloWorld(Resource):
-   def get(self):
-      # This method gets called on a GET request, pretty neato huh?
-      return jsonify({'hello': 'world'})
-
