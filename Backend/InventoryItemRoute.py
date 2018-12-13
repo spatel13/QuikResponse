1e4e5e66ee546a2067b8c16cd4a906e430488436
mikemehl <michaelalanmehl@gmail.com>
2018-12-13 17:21:27 -0500

    Added a bunch of eggs to the database.

--- Backend/InventoryItemRoute.py
+++ Backend/InventoryItemRoute.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, reqparse
 from flask import jsonify, Request
 from DbModels import Inventoryitem 
 from DbModels import database as db
-from peewee import DoesNotExist, IntegrityError
+from peewee import DoesNotExist, IntegrityError, fn
 from playhouse.shortcuts import model_to_dict
 
 class InventoryItemRoute(Resource):
@@ -38,8 +38,8 @@ class InventoryItemRoute(Resource):
          parser.add_argument('type')
          parser.add_argument('weight')
          args = parser.parse_args()
-         # Need to manually set ID due to database layout and peewee's way of handling ids.
-         # So, figure out the id to use.
+         # Figure out a good id to use for our new item.
+         max_id = Inventoryitem.select(fn.MAX(Inventoryitem.id)).scalar()
          # Load 'em into a DB model.
          entry = Inventoryitem(\
             details=args['details'],\
@@ -47,11 +47,47 @@ class InventoryItemRoute(Resource):
             name=args['name'],\
             type=args['type'],\
             weight=args['weight'],\
+            id=max_id+1
             )
-         pdb.set_trace()
          if entry.save(force_insert=True):
             # Great! Send back a copy to confirm.
-            return model_to_dict([entry])
+            # BUT...have to get the actual id (the id in the current entry gets set to the rowid in sqlite).
+            entry.id = max_id+1
+            return jsonify(model_to_dict(entry))
+         else:
+            # Not great! Let 'em know ya dun goofed.
+            pass
+      except IntegrityError:
+         return jsonify({'Error':'Ya Dun Goofed.'})
+      except:
+         pass
+      return '', 500
+
+   def put(self, lookup_by=None, criteria_1=None, criteria_2=None):
+      if lookup_by or criteria_1 or criteria_2:
+         return '', 400
+      try:
+         # Parse the arguments.
+         parser = reqparse.RequestParser()
+         parser.add_argument('details')
+         parser.add_argument('isperishable')
+         parser.add_argument('name')
+         parser.add_argument('type')
+         parser.add_argument('weight')
+         parser.add_argument('id')
+         args = parser.parse_args()
+         # Lookup the matching entry and update it's values.
+         pdb.set_trace()
+         entry = Inventoryitem.get_by_id(int(args['id']))
+         entry.details=args['details']
+         entry.isperishable=args['isperishable']
+         entry.name=args['name']
+         entry.type=args['type']
+         entry.weight=args['weight']
+         pdb.set_trace()
+         if entry.save():
+            # Great! Send back a copy to confirm.
+            return jsonify(model_to_dict(entry))
          else:
             # Not great! Let 'em know ya dun goofed.
             pass
--- Backend/test.py
+++ Backend/test.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # Dummy inventory data.
 inventory_data = {'details':'It\'s an egg.', 'isperishable':int(1), 'name':'Egg', 'type':'Egg', 'weight':'1oz'}
@@ -10,5 +11,15 @@ print("Sending...")
 r = requests.post('http://127.0.0.1:5000/api/inventory', data=inventory_data)
 
 # Did it like our egg?
+print("Received:")
+print(r.json())
+
+# Wait, we put the wrong weight for the egg!
+# Let's change it...
+inventory_data['weight'] = '1lb'
+inventory_data['id'] = json.loads(r.text)['id']
+r = requests.put('http://127.0.0.1:5000/api/inventory', data=inventory_data)
+
+# Did it update our egg?
 print("Received:")
 print(r.json())
