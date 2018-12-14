f4ad6e2d3748dbe5b8df8de33dd8805d632a4214
mikemehl <michaelalanmehl@gmail.com>
2018-12-14 17:57:03 -0500

    Added RescueRequestRoute with GET, POST, and PUT requests handled.

--- Backend/RescueRequestRoute.py
+++ Backend/RescueRequestRoute.py
@@ -2,6 +2,7 @@ import pdb
 from flask_restful import Resource, reqparse
 from flask import jsonify
 from DbModels import Rescuerequest
+from DbModels import Rescuerequester
 from DbModels import Location
 from DbModels import database as db
 from peewee import DoesNotExist, IntegrityError, fn
@@ -37,7 +38,6 @@ class RescueRequestRoute(Resource):
          parser.add_argument('severity')
          args = parser.parse_args()
          # Figure out a good id to use for our new item.
-         pdb.set_trace()
          max_id = Rescuerequest.select(fn.MAX(Rescuerequest.id)).scalar()
          # Load 'em into a DB model.
          entry = Rescuerequest(\
@@ -52,6 +52,38 @@ class RescueRequestRoute(Resource):
             # Great! Send back a copy to confirm.
             # BUT...have to get the actual id (the id in the current entry gets set to the rowid in sqlite).
             entry.id = max_id+1
+            return jsonify(model_to_dict(entry))
+         else:
+            # Not great! Let 'em know ya dun goofed.
+            pass
+      except IntegrityError:
+         return jsonify({'Error':'Ya Dun Goofed.'})
+      except:
+         pass
+      return '', 500
+
+   def put(self, lookup_by=None, criteria=None):
+      if lookup_by or criteria:
+         return '', 400
+      try:
+         # Parse the arguments.
+         parser = reqparse.RequestParser()
+         parser.add_argument('id')
+         parser.add_argument('date')
+         parser.add_argument('details')
+         parser.add_argument('locationid')
+         parser.add_argument('requesterid')
+         parser.add_argument('severity')
+         args = parser.parse_args()
+         # Lookup the matching entry and update it's values.
+         entry = Rescuerequest.get_by_id(int(args['id']))
+         entry.date=args['date']
+         entry.details=args['details']
+         entry.locationid=args['locationid']
+         entry.requesterid=args['requesterid']
+         entry.severity=args['severity']
+         if entry.save():
+            # Great! Send back a copy to confirm.
             return jsonify(model_to_dict(entry))
          else:
             # Not great! Let 'em know ya dun goofed.
--- Backend/test.py
+++ Backend/test.py
@@ -28,14 +28,24 @@ print(r.json())
 print("ROUTE TEST 2: EGGMERGENCY")
 # Eggs sure are yummy. But what if we need some? 
 # Well, I think in that case, we need to put in a rescue request!
-rescue_data = {'details':'I really want an egg.', 'date':None , 'locationid':'1', 'requesterid':'1', 'severity':'HIGHEST'}
+rescue_data = {'details':'I really want an egg.', 'date':None , 'locationid':1, 'requesterid':1, 'severity':'HIGHEST'}
 print("Data:")
 print(rescue_data)
 
 # Post that jam.
 print("Sending...")
-r = requests.post('http://127.0.0.1:5000/api/request', data=inventory_data)
+r = requests.post('http://127.0.0.1:5000/api/request', data=rescue_data)
 
 # Will they send us an egg?
+print("Received:")
+print(r.json())
+
+# Wait, we need more eggs!
+# Let's ask for more eggs.
+rescue_data['details'] = 'I really really want two eggs.'
+rescue_data['id'] = json.loads(r.text)['id']
+r = requests.put('http://127.0.0.1:5000/api/request', data=rescue_data)
+
+# Did it update our eggmergency?
 print("Received:")
 print(r.json())
